bfd127a692109a0c48eeed18e14babcabb989bdb
ci-build <ci-build@opendrift>
2020-07-01 10:24:00 +0000

    [skip ci] Updates

--- index-1.py
+++ index-1.py
@@ -1,30 +1,11 @@
-from datetime import timedelta
-from opendrift.models.openoil import OpenOil
-from opendrift.readers import reader_netCDF_CF_generic
-o = OpenOil()
-
-reader_arome = reader_netCDF_CF_generic.Reader(o.test_data_folder() + '16Nov2015_NorKyst_z_surface/arome_subset_16Nov2015.nc')
-reader_norkyst = reader_netCDF_CF_generic.Reader(o.test_data_folder() + '16Nov2015_NorKyst_z_surface/norkyst800_subset_16Nov2015.nc')
-
-o.add_reader([reader_norkyst, reader_arome])
-
-lon = 4.6; lat = 60.0; # Outside Bergen
-
-time = [reader_arome.start_time, reader_arome.start_time + timedelta(hours=30)]
-
-# Seed oil elements at defined position and time
-o.seed_elements(lon, lat, radius=50, number=3000, time=time,
-                wind_drift_factor=.02)
-
-# Adjusting some configuration
-o.set_config('processes:dispersion', False)
-o.set_config('processes:evaporation', False)
-o.set_config('processes:emulsification', True)
-o.set_config('drift:current_uncertainty', .1)
-o.set_config('drift:wind_uncertainty', 1)
-
-# Running model
-o.run(steps=60, time_step=1800)
-
-# Print and plot results
-o.plot()
+from datetime import datetime, timedelta
+from opendrift.models.oceandrift import OceanDrift
+# >>>
+o = OceanDrift()
+o.add_readers_from_list(
+    ['https://thredds.met.no/thredds/dodsC/sea/norkyst800m/1h/aggregate_be'])
+o.set_2d()
+o.seed_elements(lon=4.85, lat=60, time=datetime.now(), number=10000, radius=1000)
+# >>>
+o.run(duration=timedelta(hours=24))
+o.animation(filename='frontpage.mp4')
